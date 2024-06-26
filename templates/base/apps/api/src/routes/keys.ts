eccca9df02403bcbc6129d630362d1d0f0636e21
James Perkins <james@unkey.dev>
2024-06-26 18:27:31 -0400

    Adding keys docs
    Making sure keys have good errors

--- templates/base/apps/api/src/routes/keys.ts
+++ templates/base/apps/api/src/routes/keys.ts
@@ -7,7 +7,13 @@ import { createKey } from "../schema/keys";
 export const keys = new OpenAPIHono<HonoEnv>().openapi(createKey, async (c) => {
   const ratelimited = await c.get("ratelimit").limit("fake-user");
   if (!ratelimited.success) {
-    return c.json({ error: "Try again later" }, 429);
+    return c.json(
+      {
+        message: "Please try again later",
+        docs: "http://localhost:3000/docs/api-reference",
+      },
+      429
+    );
   }
   const unkey = new Unkey({ rootKey: c.env.UNKEY_ROOT_KEY });
   const apiId = c.env.UNKEY_API_ID!;
@@ -16,7 +22,7 @@ export const keys = new OpenAPIHono<HonoEnv>().openapi(createKey, async (c) => {
     prefix: "tool",
   });
   if (error) {
-    return c.json({ error: "Error occcured creating key" }, 400);
+    return c.json({ message: error.message, docs: error.docs }, 500);
   }
   return c.json({ key: result.key, keyId: result.keyId }, 200);
 });
