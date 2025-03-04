5473489b96da68d92317ffb9a4edf82fb3dd8e4e
James Perkins <james@unkey.dev>
2025-03-04 12:53:17 -0500

    latest

--- src/installers/dependencyVersionMap.ts
+++ src/installers/dependencyVersionMap.ts
@@ -4,16 +4,16 @@
  */
 export const dependencyVersionMap = {
   // Prisma
-  prisma: "^6.4.1",
-  "@prisma/client": "^6.4.1",
-  "@prisma/adapter-libsql": "^6.4.1",
+  prisma: "^5.14.0",
+  "@prisma/client": "^5.14.0",
+  "@prisma/adapter-libsql": "^5.15.0",
   // Drizzle
-  "drizzle-orm": "^0.40.0",
-  "drizzle-kit": "^0.30.5",
+  "drizzle-orm": "^0.30.10",
+  "drizzle-kit": "^0.21.4",
   "eslint-plugin-drizzle": "^0.2.3",
   // These are dependencies that are shared between Prisma and Drizzle
   "dotenv-cli": "^7.4.2",
-  "@libsql/client": "^0.14.0",
+  "@libsql/client": "^0.6.2",
   // PostgreSQL dependencies
   pg: "^8.11.3",
   "@types/pg": "^8.10.9",
